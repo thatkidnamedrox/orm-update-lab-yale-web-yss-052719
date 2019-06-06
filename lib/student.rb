f499ca2d7d7af1531128fbbe031ce1e3e28f9307
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 15:32:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,25 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def initialize(name, grade)
+    @id = nil
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  
+
 
 end
