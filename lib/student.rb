88d2ce61fbdf9af4de164628741acf3b7b45a61f
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 15:43:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,6 +41,13 @@ class Student
     student = Student.new(row[1], row[2], row[0])
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name=?
+    SQL
+    DB[:conn].execute(sql, name)
+  end
   def save
     if self.id
       self.update
